84ea141668397aff49f4367b60a28b13f752fa0a
Mike <ghettosoak@gmail.com>
2013-05-26 01:12:52 +0200

    Backend Complete? pt2
    damn. forgot something. where were we? ah yes – hooking everything up. that's next. :)

--- front/js/me.js
+++ front/js/me.js
@@ -5,25 +5,8 @@ $('.who').on('click', function(){
 })
 
 function iammike(){
-	var bestside = Math.floor(Math.random() * (10 - 1) + 1)
-	// $('.me').css('background','url(img/me/'+bestside+'.jpg) no-repeat center center fixed');
 	$('#me_clean').append('<img src="img/me/clean/'+bestside+'.jpg" />')
 	$('#me_blur').append('<img src="img/me/blur/'+bestside+'.jpg" />')
-	// $('#megrey').css({
-	// 	'top': ($('.meme').outerHeight(true)-200)/2,
-	// 	'left': ($('.meme').outerWidth(true)-200)/2
-	// })
-	// $('.meback').find('img').css('margin-top', ($('.meback').outerHeight(true)-52)/2)
-	// $('#mebackwhite').css({
-	// 	'top': ($('.meback').outerHeight(true)-160)/2,
-	// 	'left': ($('.meback').outerWidth(true)-160)/2
-	// })
-	// $('.meblur').blurjs({
-	// 	source: '.me',
-	// 	radius: 7,
-	// 	cache: true,
-	// 	overlay: 'rgba(255,255,255,0.4)'
-	// });
 }
 
 $('.me').on('click', '.me_bit', function(){
